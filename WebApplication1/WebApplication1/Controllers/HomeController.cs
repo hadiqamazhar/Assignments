6b5c1fc6547506f9e7a62de99d141b6f0ca1ac39
Hadiqa <hadiqamazhar49@gmail.com>
2020-05-28 03:08:55 +0500

    LMS

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataClasses2DataContext dc = new DataClasses2DataContext();
+
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult AddBooks()
+        {
+            return View();
+        }
+
+        public ActionResult ViewBooks(string searchBy,string search)
+        {
+            if(searchBy=="Name")
+            {
+                return View(dc.books.Where(x=>x.name==search || search==null).ToList());
+            }
+            else
+            {
+                return View(dc.books.Where(x => x.author == search || search==null).ToList());
+            }
+        }
+
+        public ActionResult Add()
+        {
+            string bookName = Request["bookname"];
+            string author = Request["author"];
+            string genre = Request["genre"];
+
+            book b = new book();
+            b.name = bookName;
+            b.author = author;
+            b.genre = genre;
+
+            dc.books.InsertOnSubmit(b);
+            dc.SubmitChanges();
+
+            return RedirectToAction("ViewBooks");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var selection = dc.books.First(c => c.Id == id);
+            dc.books.DeleteOnSubmit(selection);
+            dc.SubmitChanges();
+            
+            return RedirectToAction("ViewBooks");
+        }
+
+        public ActionResult EditBook(int id)
+        {
+            return View(dc.books.First(c=>c.Id==id));
+        }
+
+        public ActionResult EditDone(int id)
+        {
+            var selection = dc.books.First(c => c.Id == id);
+            selection.name = Request["bookname"];
+            selection.author = Request["author"];
+            selection.genre = Request["genre"];
+            dc.SubmitChanges();
+
+            return RedirectToAction("ViewBooks");
+        }
+    }
+}
